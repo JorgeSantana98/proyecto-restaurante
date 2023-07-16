15e5e4e820a2168d591465296dab1f02a2061b97
JorgeSantana98 <jorge28398@hotmail.com>
2023-07-16 12:57:07 -0500

    agregue todos los archivos por primera vez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContenidoInicioComponent } from './components/contenido-inicio/contenido-inicio.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: ContenidoInicioComponent},
+  {path: 'login',component: LoginComponent},
+  {path:'about',component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { LoginComponent } from './components/login/login.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContenidoInicioComponent } from './components/contenido-inicio/contenido-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    LoginComponent,
+    PiePaginaComponent,
+    AboutComponent,
+    ContenidoInicioComponent
   ],
   imports: [
     BrowserModule,
